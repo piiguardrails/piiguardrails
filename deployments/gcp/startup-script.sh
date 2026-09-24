cb224940b53d05890a7fdb5b1dde42b36315f7f7
VISHAL PRAJAPATI <75498340+prajapativ@users.noreply.github.com>
2026-09-24 14:07:06 +0530

    feat(gcp): sync deployments/gcp/startup-script.sh

--- deployments/gcp/startup-script.sh
+++ deployments/gcp/startup-script.sh
@@ -35,7 +35,7 @@ if [ -z "$GUI_PASSWORD" ]; then
     echo "Generated secure random GUI password."
 fi
 COMPANY_NAME=$(get_metadata "company_name" "GCP Demo")
-BRAND_LOGO=$(get_metadata "brand_logo" "")
+BRAND_LOGO=$(get_metadata "brand_logo" "/ui/logo.png")
 DEFAULT_TRIAL_KEY="ED4-AQAGWNU7FAAAAJYQ-AAAAAAAAAAAAAKFI-VXRAU4YPAHXPW6JD-CRJIDR5NFJ4ITZA2-ICFCAEEV7OYVNWO7-JDSSZ5RMIUNDHULQ-DMZPYP4ICOFTJF53-AMKICAI6R57R4XM4-QYR4UGEQ4ZTP3DAM"
 LICENSE_KEY=$(get_metadata "license_key" "$DEFAULT_TRIAL_KEY")
 APP_PORT=$(get_metadata "app_port" "8000")
